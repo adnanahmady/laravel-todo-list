d511fe246f0da628ba8d8df128d6f39e9a597b56
adnanahmady <adnan.ahmady1394@gmail.com>
2019-07-12 18:30:52 +0430

    add documentation

--- app/Task.php
+++ app/Task.php
@@ -1,11 +1,23 @@
 <?php
-
+/**
+ * @author adnan ahmady <adnan.ahmady1394@gmail.com>
+ */
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Task Model
+ * 
+ * @package App
+ */
 class Task extends Model
 {
+    /**
+     * set user one to many relationship
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user() {
         return $this->belongsTo(User::class);
     }
