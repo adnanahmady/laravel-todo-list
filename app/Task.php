ab5799d59aef9a31d3448d23e19e199583ff9f8b
adnanahmady <adnan.ahmady1394@gmail.com>
2019-07-12 18:32:01 +0430

    override find method

--- app/Task.php
+++ app/Task.php
@@ -5,10 +5,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 /**
  * Task Model
- * 
+ *
  * @package App
  */
 class Task extends Model
@@ -20,5 +21,22 @@ class Task extends Model
      */
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Override find method
+     *
+     * @param $taskId
+     * @param null $username
+     * @return Task|Task[]|\Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection|Model|null
+     */
+    public static function find($taskId, $username = null) {
+        $task = static::with('user')->find($taskId);
+
+        if ($username and @$task->user->username !== $username) {
+            throw new ModelNotFoundException();
+        }
+
+        return $task;
     }
 }
