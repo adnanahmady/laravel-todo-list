3e34bc5ee59858f121a44b33d6f2d4befa5ef4cf
adnanahmady <adnan.ahmady1394@gmail.com>
2019-07-13 21:19:04 +0430

    Add Custom Validation to Tasks Controller

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -4,10 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Task;
 use Illuminate\Http\Request;
+use Custom\Exceptions\ValidatorException;
+use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\View;
+use Custom\Services\TaskCreatorService;
+use Custom\Validators\TaskValidator;
 
 class TasksController extends Controller
 {
+    protected $taskCreator;
+
+    public function __construct()
+    {
+        $this->taskCreator = new TaskCreatorService(new TaskValidator());
+    }
+
     public function index() {
         $tasks = Task::with('user')->get();
 
@@ -18,5 +29,15 @@ class TasksController extends Controller
         $task = Task::with('user')->findOrFail($id);
 
         return View::make('tasks.show', compact('task'));
+    }
+
+    public function store() {
+        try {
+            $this->taskCreator->make(Input::all());
+        } catch (ValidatorException $exception) {
+            return redirect()->back()->withInput()->withErrors($exception->getErrors());
+        }
+
+        return redirect()->route('tasks');
     }
 }
