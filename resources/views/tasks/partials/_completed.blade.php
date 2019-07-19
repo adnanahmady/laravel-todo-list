663466fd689128e5fae8898d2e674b12ed13e763
adnanahmady <adnan.ahmady1394@gmail.com>
2019-07-19 21:07:18 +0430

    Add a completed section using form open

---
+++ resources/views/tasks/partials/_completed.blade.php
@@ -0,0 +1,15 @@
+{{ Form::open([
+    'method' => 'PATCH',
+    'url' => "tasks/update/$task->id"
+]) }}
+{{ csrf_field() }}
+<label class="btn btn-outline-secondary m-0 completed-label" for="completed-{{ $task->id }}">
+    <span class="check-completed {{ $task->completed ? 'text-success' : 'text-danger' }}">
+        <i
+            class="fa fa-{{ $task->completed ? 'check' : 'times'}}"
+            aria-hidden="true"
+        ></i>
+    </span>
+</label>
+{{ Form::checkbox('completed', null, $task->completed, ['class' => 'completed', 'id' => 'completed-' . $task->id]) }}
+{{ Form::close() }}
